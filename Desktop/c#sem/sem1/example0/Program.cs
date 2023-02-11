7192e0a4dc0996a6ba268021c4de485d265bce96
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-02-11 22:52:03 +0300

    first example

---
+++ Desktop/c#sem/sem1/example0/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа, которая на вход принимает число и выдает его квадрат
+
+Console.WriteLine("Введите число");
+
+string answer = Console.ReadLine();
+int number = Convert.ToInt32(answer);
+
+Console.WriteLine(number * number);
+
