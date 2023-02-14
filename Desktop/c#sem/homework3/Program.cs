566b07e1ba1b029b7cbd19e0d9034ac0eceee5e1
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-02-14 20:34:08 +0300

    task 3

---
+++ Desktop/c#sem/homework3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string? answer = Console.ReadLine();
+int na = Convert.ToInt32(answer);
+
+if (na % 2 == 0)
+
+{
+    Console.WriteLine ("Число четное");
+}
+else Console.WriteLine ("Число нечетное");
+
