f639041687f57a3f8085effbe042769acb3c36d1
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-02-14 20:34:14 +0300

    task 4

---
+++ Desktop/c#sem/homework4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+string? answer = Console.ReadLine();
+int n = Convert.ToInt32(answer);
+int i = 1;
+if (i <= n)
+{
+
+i++;
+ while (i % 2 == 0)
+
+ {
+    
+    Console.WriteLine(i);
+ }   
+
+
+}
