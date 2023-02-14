e5371e12fa37f2c08c1334108a0dbdafdfc69daf
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-02-14 22:16:54 +0300

    homework 4

--- Desktop/c#sem/homework4/Program.cs
+++ Desktop/c#sem/homework4/Program.cs
@@ -2,16 +2,16 @@
 string? answer = Console.ReadLine();
 int n = Convert.ToInt32(answer);
 int i = 1;
-if (i <= n)
+while (i <= n)
 {
 
-i++;
- while (i % 2 == 0)
+ if (i % 2 == 0)
 
  {
     
     Console.WriteLine(i);
+    i++;
  }   
-
+else i++;
 
 }
