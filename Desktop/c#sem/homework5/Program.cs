4f41ef87c90b36a1c563a88e5c48550b4ebeb2d5
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-02-14 22:47:02 +0300

    homework 5,7

---
+++ Desktop/c#sem/homework5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число"); 
+string? answer = Console.ReadLine();     
+int na = Convert.ToInt32(answer);
+int result = - 1;
+
+if (na > 99 && na< 1000)
+
+{
+    while (na > 99)
+    {
+        na = na / 10;
+
+    }
+    result = na % 10;
+    Console.Write(result);
+}
+else Console.WriteLine ("Введено неправильное число");
---
+++ Desktop/c#sem/homework7/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите число");
+
+string? answer = Console.ReadLine();
+int n = Convert.ToInt32(answer);
+
+if (n == 1)Console.WriteLine("Monday");
+if (n == 2)Console.WriteLine("Tuesday");
+if (n == 3)Console.WriteLine("Wednesday");
+if (n == 4)Console.WriteLine("Thursday");
+if (n == 5)Console.WriteLine("Friday");
+if (n == 6)Console.WriteLine("Saturday");
+if (n == 7)Console.WriteLine("Sunday");
+if (n > 7) Console.WriteLine("Time paradox");
+if (n > 5 && n < 8) Console.WriteLine("Holiday. Stay home and rest");
+if (n > 0 && n < 6) Console.WriteLine("Go to work");
