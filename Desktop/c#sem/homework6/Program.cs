9a95aacdca3db7e114aae8d49b884818b4f9c4ae
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-02-14 21:57:22 +0300

    homework

---
+++ Desktop/c#sem/homework6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число"); 
+string? answer = Console.ReadLine();     
+int na = Convert.ToInt32(answer);
+int result = - 1;
+
+if (na > 99)
+
+{
+    while (na > 999)
+    {
+        na = na / 10;
+
+    }
+    result = na % 10;
+    Console.Write(result);
+}
+else Console.WriteLine ("Нет такого числа");
+    
