ddb8e50ea446517dad0e5d92b61e387f4b2a7523
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-02-14 20:34:00 +0300

    task 2

---
+++ Desktop/c#sem/homework2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите первое число");
+string? answer = Console.ReadLine();
+int na = Convert.ToInt32(answer);
+
+Console.WriteLine("Введите второе число");
+
+string? answer2 = Console.ReadLine();
+int nb = Convert.ToInt32(answer2);
+
+Console.WriteLine("Введите третье число");
+
+string? answer3 = Console.ReadLine();
+int nc = Convert.ToInt32(answer3);
+
+if (na>nb)
+{
+    Console.Write("max = ");
+    Console.WriteLine (na);
+   
+}
+
+if (nc>na)
+{
+        Console.Write("max = ");
+    Console.WriteLine (nc);
+  
+}
+
+if (nb>nc)
+{
+        Console.Write("max = ");
+    Console.WriteLine (nb);
+   
+}
