97c09e75a479a3059a7a292b64bffb11613bfcb5
Eldar Gadzhiev <mrcartman65@gmail.com>
2023-03-02 23:43:09 +0300

    homework corrected2

--- Desktop/c#sem/specialtask41/Program.cs
+++ Desktop/c#sem/specialtask41/Program.cs
@@ -1,29 +1,29 @@
 ﻿int count = 0;
 int n;
 string stop = "stop";
-Console.WriteLine("введите число"); 
+Console.WriteLine("введите число");
 
-    while (true) 
-    { 
-  
+while (true)
+{
+
     var data = Console.ReadLine();
-    if (String.Equals(data, stop, StringComparison.CurrentCultureIgnoreCase)) 
+    if (String.Equals(data, stop, StringComparison.CurrentCultureIgnoreCase))
     {
-      break;
+        break;
     }
-     
 
-    
-  
-    
+
+
+
+
     if (int.TryParse(data, out n))
     {
-      if (Convert.ToInt32(data) > 0)
-      {
-        count++;
-      }
-      
+        if (Convert.ToInt32(data) > 0)
+        {
+            count++;
+        }
+
     }
     else Console.WriteLine("Неправильное стоп-слово");
-    } 
-  Console.WriteLine($"Количество чисел, больше нуля = {count}");
+}
+Console.WriteLine($"Количество чисел, больше нуля = {count}");
